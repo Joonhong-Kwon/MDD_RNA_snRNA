7fd2b04d6fb1c5ac4aae4ed199a5cb09a350ea3f
Joonhong-Kwon <96454041+Joonhong-Kwon@users.noreply.github.com>
2026-02-04 16:06:11 +0900

    Delete RNAseq directory

--- RNAseq/Quantile_Normalization.R
+++
@@ -1,9 +0,0 @@
-library(preprocessCore)
-
-
-FPKM <- read.csv("Before_Quan.csv")
-FPKM_mat <- data.matrix(FPKM)
-
-FPKM_nor <- normalize.quantiles(FPKM_mat[,-c(1:4)], copy=TRUE)
-
-write.csv(FPKM_nor, "After_Quan.csv")
